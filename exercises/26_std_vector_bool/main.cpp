bb87fd207fbc3ab1b1feeac1e73b4b1adb2c8b1e
Qiushi Du <qs.du@high-flyer.cn>
2025-01-20 17:10:12 +0800

    update CXX

--- exercises/26_std_vector_bool/main.cpp
+++ exercises/26_std_vector_bool/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv) {
     ASSERT(vec.size() == 100, "Make this assertion pass.");
     // NOTICE: 平台相关！注意 CI:Ubuntu 上的值。
     std::cout << "sizeof(std::vector<bool>) = " << sizeof(std::vector<bool>) << std::endl;
-    ASSERT(sizeof(vec) == 24, "Fill in the correct value.");
+    printf("%d", sizeof(vec));
+    ASSERT(sizeof(vec) == 40, "Fill in the correct value.");
     {
         vec[20] = false;
         ASSERT(!vec[20], "Fill in `vec[20]` or `!vec[20]`.");
@@ -23,14 +24,11 @@ int main(int argc, char **argv) {
         ASSERT(!vec[100], "Fill in `vec[100]` or `!vec[100]`.");
     }
     {
-        // std::vector<bool>的operator[]返回一个代理对象(proxy object)而不是bool&
-        // 这个代理对象可以修改底层的bool值
-        // 所以当我们通过ref修改值为false时,实际上修改了vec[30]的值
-        auto ref = vec[30];// ref是代理对象,此时vec[30]为true
+        auto ref = vec[30];
         ASSERT(ref, "Fill in `ref` or `!ref`");
-        ref = false;// 通过代理对象修改vec[30]的值为false
+        ref = false;
         ASSERT(!ref, "Fill in `ref` or `!ref`");
-        // vec[30]已经被修改为false
+        // THINK: WHAT and WHY?
         ASSERT(!vec[30], "Fill in `vec[30]` or `!vec[30]`.");
     }
     return 0;
